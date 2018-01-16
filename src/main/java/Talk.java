4cc6592c2a218a5a4ebb22a856fe4cc8e8487ee9
Jan Augustyniak <janek.augustyniak@gmail.com>
2018-01-16 21:46:36 +0100

    Get classes fix

--- src/main/java/Talk.java
+++ src/main/java/Talk.java
@@ -11,7 +11,7 @@ import org.eclipse.rdf4j.repository.http.HTTPRepository;
 public class Talk {
 	String rdf4jServer = "http://77.55.220.23:8080//rdf4j-server/";
 	String repositoryID = "family-memory-rdfs";
-	String getOntologyClassesQuery = "PREFIX bc: <http://a.com/ontology#> SELECT DISTINCT ?type WHERE {?subject a ?type.FILTER( STRSTARTS(STR(?type),str(bc:)) )}";
+	String getOntologyClassesQuery = "PREFIX bc: <http://a.com/ontology#> SELECT DISTINCT ?type WHERE { ?type a owl:Class FILTER ( STRSTARTS(STR(?type),str(bc:)))}";
 	String getAllClassesQuery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> SELECT DISTINCT ?type WHERE {?s a ?type.}";
 	Repository repo;
 	
