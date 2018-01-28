ea74f20cecb60b6898ca728400f96bc7e79f86a0
Jan Augustyniak <janek.augustyniak@gmail.com>
2018-01-28 13:27:02 +0100

    Enable CORS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,7 +30,7 @@ public class Main {
     }
     
     public static void main(String[] args) {
-    	//enableCORS("*","*","*");
+    	enableCORS("*","*","*");
         talk = new Talk();
         talk.LoadRepository();
 
